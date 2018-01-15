fe1b9c1ce58e6afabcefcb860b1ac7a540ded602
circle <flower_jing@163.com>
2018-01-15 18:09:53 +0800

    Update 将一个任意长的数字变成逗号分割的格式.js

--- 实践题/将一个任意长的数字变成逗号分割的格式.js
+++ 实践题/将一个任意长的数字变成逗号分割的格式.js
@@ -1,3 +1,13 @@
 // 1234.56 => "1,234.56" , 123456789 => "123,456,789"
 // parseToMoney(1234.56) return "1,234.56"
 
+  function parseToMoney (val) {
+      var valSp= val.split('.');
+      var valPre= valSp[0];
+      var valAft = valSp.length>1 ? '.'+ valSp[1]:'';
+      var re=/(\d+)(\d{3})/;
+      while(re.test(valPre)) {
+          valPre=valPre.replace(re,'$1'+','+'$2');
+      }
+      return valPre+valAft;          
+  }

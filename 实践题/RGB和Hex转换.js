8d7352de61ab1f79cc3f0cce9d61715ce91a9ddf
circle <flower_jing@163.com>
2018-01-17 16:27:32 +0800

    Update RGB和Hex转换.js

--- 实践题/RGB和Hex转换.js
+++ 实践题/RGB和Hex转换.js
@@ -8,3 +8,16 @@
  rgb(0,0,0) // returns 000000
  rgb(148, 0, 211) // returns 9400D3
  */
+
+function rgb(x,y,z) {
+    var color=[x,y,z];
+    var hex = "#";
+
+    for (var i = 0; i < 3; i++) {
+        // 'Number.toString(16)' 是JS默认能实现转换成16进制数的方法.
+        // 如果结果是一位数，就在前面补零。例如： A变成0A
+        hex += ("0" + color[i].toString(16)).slice(-2);
+    }
+    return hex;
+};
+var rg=rgb(125,125,125);  //#7d7d7d

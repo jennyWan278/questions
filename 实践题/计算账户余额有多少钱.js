5e282a4b70427556d1c9f5ff345a043f7081ce43
circle <flower_jing@163.com>
2018-01-19 16:30:47 +0800

    Update 计算账户余额有多少钱.js

--- 实践题/计算账户余额有多少钱.js
+++ 实践题/计算账户余额有多少钱.js
@@ -1,3 +1,11 @@
-var string = "我的账户余额：2,235,467.20";
+var string = "我的账户余额:2,235,467.20";
 // console.log(?);
 // 请用js计算出我到底有多少钱（输出Number类型数字，代码尽量简洁，考虑通用情况）
+
+var s= "我的账户余额:2,235,467.20";
+var i=s.indexOf(':');
+var m=s.substring(i+1);
+var arr=m.split(',');
+var nums=arr.join('');
+var num=Number(nums);
+console.log(num);
